66fcdcba25b717e200d1362dd8c9d1e139966644
tetiana_baranova <syniovych.tanya@gmail.com>
2026-04-10 14:33:01 +0300

    add FloatLayout

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import  ObjectProperty
 
 class Container(BoxLayout):
-    pass
+    text_input = ObjectProperty()
+    label_text = ObjectProperty()
+
+    def change_something(self):
+        self.label_text.text = self.text_input.text
 
 class DuckyApp(App):
     def build(self):
