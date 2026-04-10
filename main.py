98d5dd2a523666dd4732eff683dbcc70e934718c
tetiana_baranova <syniovych.tanya@gmail.com>
2026-04-10 14:16:33 +0300

    add container

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+
+class Container(BoxLayout):
+    pass
 
 class DuckyApp(App):
     def build(self):
-        box = BoxLayout()
-        b1 = Button(text="Hello all")
-        b2 = Button(text="Hello")
-        box.add_widget(b1)
-        box.add_widget(b2)
-        return box
+        return Container()
 
 if __name__ == '__main__':
     DuckyApp().run()
