f6fd142372e555ef92b8582e18b90f02cfd8013a
tetiana_baranova <syniovych.tanya@gmail.com>
2026-04-10 23:19:14 +0300

    time converter

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import  ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+from kivy.core.window import Window
 
-class Container(BoxLayout):
-    text_input = ObjectProperty()
-    label_text = ObjectProperty()
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+Window.size = (560, 640)
+class Container(GridLayout):
+    def convert(self):
+        try:
+            number = int(self.text_input.text)
+        except Exception:
+            number = 0
+        self.label_hours.text = str(number * 24)
+        self.label_minutes.text = str(number * 24 * 60)
+        self.label_seconds.text = str(number * 24 * 60 * 60)
+        self.label_m_seconds.text = str(number * 24 * 60 * 60 * 60)
+        self.label_weeks.text = str("%.2f" % round(number / 7, 2))
 
-    def change_something(self):
-        self.label_text.text = self.text_input.text
+
 
 class DuckyApp(App):
     def build(self):
